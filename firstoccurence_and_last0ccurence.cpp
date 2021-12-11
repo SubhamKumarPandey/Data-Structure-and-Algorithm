94bdb091e4a39b600dc003912b4e99d2d20d1106
Subham <subhampandeywbjee12@gmail.com>
2021-12-11 13:53:12 +0530

    first and last occurence in sorted array

---
+++ firstoccurence_and_last0ccurence.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+int fi(int arr[],int n,int k){
+	int e=n-1;int s=0;
+	int ans=-1;
+	int mid=s+(e-s)/2;
+	while(s<=e){
+		if(arr[mid]==k){
+			ans=mid;
+			e=mid-1;
+		}
+		else if(k>arr[mid]){
+			s=mid+1;
+		}
+		else{
+			e=mid-1;
+		}
+		mid=s+(e-s)/2;
+	}
+	return ans;
+}
+int si(int arr[],int n,int k){
+	int e=n-1;int s=0;
+	int ans=-1;
+	int mid=s+(e-s)/2;
+	while(s<=e){
+		if(arr[mid]==k){
+			ans=mid;
+			s=mid+1;
+		}
+		else if(k>arr[mid]){
+			s=mid+1;
+		}
+		else{
+			e=mid-1;
+		}
+		mid=s+(e-s)/2;
+	}
+	return ans;
+}
+int main(){
+		int n=5;
+	int arr[n]={1,4,4,4,5};
+	int key=4;
+	int ans1=fi(arr,n,key);
+	int ans2=si(arr,n,key);
+	cout<<ans1<<endl<<ans2;
+
+}
