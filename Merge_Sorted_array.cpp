57003fe263a8f54e984cdfa25f683948aae0614f
Subham <subhampandeywbjee12@gmail.com>
2021-12-27 22:20:44 +0530

    Merege Sorted Arrays

---
+++ Merge_Sorted_array.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+ // } Driver Code Ends
+
+class Solution{
+    public:
+        //Function to merge the arrays.
+        void merge(long long arr1[], long long arr2[], int n, int m) 
+        { 
+            int x=n-1, y = 0;
+while(x>=0 && y<m){ if(arr1[x]>=arr2[y]){
+swap(arr1[x] , arr2[y]);
+x--;
+y++;
+}
+else{
+x--;
+}
+}
+sort(arr1 , arr1+n);
+sort(arr2 , arr2+m);
+            // code here 
+        } 
+};
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    int n, m;
+	    cin >> n >> m;
+	    
+	    long long arr1[n], arr2[m];
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<m;i++){
+	        cin >> arr2[i];
+	    }
+	    Solution ob;
+	    ob.merge(arr1, arr2, n, m); 
+
+        for (int i = 0; i < n; i++) 
+            cout<<arr1[i]<<" "; 
+        
+       
+	    for (int i = 0; i < m; i++) 
+		    cout<<arr2[i]<<" "; 
+	    
+	    cout<<endl;
+	}
+
+	return 0; 
+} 
