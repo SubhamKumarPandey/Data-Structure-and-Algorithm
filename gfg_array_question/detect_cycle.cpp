6acfcb3d43c7e3e7d354e24dc06093089f48fb68
Subham <subhampandeywbjee12@gmail.com>
2022-02-02 21:22:57 +0530

    Detect Loop

---
+++ gfg_array_question/detect_cycle.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head==NULL){
+            return false;
+        }
+        ListNode* fast=head->next;
+        ListNode* slow=head;
+        while(fast!=NULL && fast->next!=NULL){
+            if(slow==fast){
+                return true;
+            }
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return false;
+        
+    }
+};
